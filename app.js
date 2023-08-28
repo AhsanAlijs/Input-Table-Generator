5dd34892392e2d383d0fa256e2212f455537bf91
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-08-28 10:58:12 -0700

    Add a media Screen and condition

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
-
-
-
-function genrate(){
+function genrate() {
     const input = document.getElementById("inputTable").value;
     const tableDisplay = document.getElementById("table-div");
     tableDisplay.innerHTML = ''
-for(let i = 1;i <= 10; i++){
-    tableDisplay.innerHTML += `${input} x ${i} = ${i*input} <br/>`;
-}
+    if (input < 1) {
+        alert("Enter A Correct Input");
+
+    } else {
+        for (let i = 1; i <= 10; i++) {
+            tableDisplay.innerHTML += `${input} x ${i} = ${i * input} <br/>`;
+        }
+    }
 }
